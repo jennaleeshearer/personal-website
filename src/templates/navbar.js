e57672f606afdb12b4b2ec1888900ac2e3c64c99
Jenna-Lee Shearer <shearerjennalee@gmail.com>
2024-09-10 14:15:17 +0200

    nav

--- src/templates/navbar.js
+++ src/templates/navbar.js
@@ -9,16 +9,22 @@ export default class CustomNavbar extends React.PureComponent {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse text-right" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link" href="#experience">Experience</a>
+                <a className="nav-link" href="#experience">
+                  Experience
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#education">Education</a>
+                <a className="nav-link" href="#education">
+                  Education
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#contact">Contact</a>
+                <a className="nav-link" href="#contact">
+                  Contact
+                </a>
               </li>
             </ul>
           </div>
